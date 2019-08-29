8b73927bf4c0f62e97ccc95683792a2804bfdb7e
nealrobben <neal.robben@cegeka.com>
2019-08-29 11:55:47 +0200

    Added redirect after login

--- ng-fundamentals/src/app/user/login.component.ts
+++ ng-fundamentals/src/app/user/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './login.component.html'
@@ -9,9 +10,15 @@ export class LoginComponent{
     userName:string;
     password:string;
 
-    constructor(private authService:AuthService){}
+    constructor(private authService:AuthService, private router:Router){}
 
     login(formValues){
         this.authService.loginUser(formValues.userName,formValues.password);
+
+        this.router.navigate(['events']);
+    }
+
+    cancel(){
+        this.router.navigate(['events']);
     }
 }
