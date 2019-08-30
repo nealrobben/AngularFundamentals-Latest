f6444348ce5cada21611dc485ace5bc93c068fd2
nealrobben <neal.robben@cegeka.com>
2019-08-30 17:31:03 +0200

    Added custom validator

--- ng-fundamentals/src/app/events/event-details/create-session.component.ts
+++ ng-fundamentals/src/app/events/event-details/create-session.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ISession } from '../shared/index';
+import { ISession, restrictedWords } from '../shared/index';
 
 @Component({
     templateUrl: './create-session.component.html',
@@ -28,7 +28,7 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('',Validators.required);
         this.duration = new FormControl('',Validators.required);
         this.level = new FormControl('',Validators.required);
-        this.abstract = new FormControl('',[Validators.required, Validators.maxLength(400)]);
+        this.abstract = new FormControl('',[Validators.required, Validators.maxLength(400), restrictedWords(['foo','bar'])]);
 
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -52,4 +52,16 @@ export class CreateSessionComponent implements OnInit {
         console.log(session);
     }
 
+    private restrictedWords(words){
+        return (control: FormControl): {[key: string]: any} => {
+            
+            if(!words) return null;
+
+            var invalidWords = words.map(w => control.value.includes(w) ? w : null).filter(w => w != null);
+            
+            return invalidWords && invalidWords.length > 0
+                ? {'restricted': invalidWords.join(', ')}
+                : null;
+        }
+    }
 }
