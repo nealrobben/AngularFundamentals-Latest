89faf373a9ba6f389c975513834a3be79cd33636
nealrobben <neal.robben@cegeka.com>
2019-09-03 15:16:35 +0200

    Added sorting (Sorting is not working, just moving on with the course)

--- ng-fundamentals/src/app/events/event-details/session-list.component.ts
+++ ng-fundamentals/src/app/events/event-details/session-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared/index';
+import { SIGUSR1, SIGUSR2 } from 'constants';
 
 @Component({
     selector: 'session-list',
@@ -9,12 +10,14 @@ export class SessionListComponent implements OnChanges{
     @Input() sessions:ISession[];
 
     @Input() filterBy:string;
+    @Input() sortBy:string;
 
     visibleSessions:ISession[] = [];
 
     ngOnChanges(): void {
         if(this.sessions){
             this.filterSessions(this.filterBy);
+            this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortyByVotesDesc);
         }
     }
 
@@ -28,4 +31,14 @@ export class SessionListComponent implements OnChanges{
             })
         }
     }
+}
+
+function sortByNameAsc(s1: ISession, s2:ISession){
+    if(s1.name > s2.name) return 1;
+    else if(s1.name === s2.name) return 0;
+    else return -1;
+}
+
+function sortyByVotesDesc(s1: ISession, s2:ISession){
+    return s2.voters.length - s1.voters.length;
 }
