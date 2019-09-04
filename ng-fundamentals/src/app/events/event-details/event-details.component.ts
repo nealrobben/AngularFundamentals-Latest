68913fbc1892e46d0ef8751439acd1ab5624c79d
nealrobben <neal.robben@cegeka.com>
2019-09-04 12:57:13 +0200

    Fixed routing bug

--- ng-fundamentals/src/app/events/event-details/event-details.component.ts
+++ ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/events.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { IEvent, ISession } from '../shared/event.model';
 
 @Component({
@@ -21,7 +21,10 @@ export class EventDetailsComponent implements OnInit {
     constructor(private eventService:EventService, private route:ActivatedRoute){}
 
     ngOnInit(): void {
-        this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+
+        this.route.params.forEach((params:Params) => {
+            this.event = this.eventService.getEvent(+params['id']);
+        })
     }
 
     addSession(){
