5b599a0f514454a16f0f36e812844d9188512b43
nealrobben <neal.robben@cegeka.com>
2019-09-04 16:50:05 +0200

    Cleaned up code

--- ng-fundamentals/src/app/events/event-details/event-details.component.ts
+++ ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -22,8 +22,8 @@ export class EventDetailsComponent implements OnInit {
 
     ngOnInit(): void {
 
-        this.route.params.forEach((params:Params) => {
-            this.event = this.route.snapshot.data['event'];
+        this.route.data.forEach((data) => {
+            this.event = data['event'];
             this.addMode = false;
         })
     }
